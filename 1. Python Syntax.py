d0c8bfb5b5bca41b5f22bc1407612436ad91c836
Jessie Setiady <23516064@std.stei.itb.ac.id>
2018-01-26 10:33:46 +0700

    Add Python Syntax Sample Code

---
+++ 1. Python Syntax.py
@@ -0,0 +1,20 @@
+print "Hello World"
+
+# Variable
+nama = "Joko Widodo"
+umur = 23
+umur_dalam_hexadesimal = 0x17
+diskon = 0.15
+is_mahasiswa = True
+
+# Komentar dalam satu baris
+
+'''
+komentar dalam beberapa baris
+'''
+
+# Operasi Aritmetika
+harga_total = 5000
+diskon = 1000
+total_bayar = harga_total - (harga_total * diskon)
+print total_bayar
