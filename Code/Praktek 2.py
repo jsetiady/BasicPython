00744d801f64cd2483f0a1b28923dafeacd03fdf
Jessie Setiady <23516064@std.stei.itb.ac.id>
2018-01-28 01:30:41 +0700

    Update readme

---
+++ Code/Praktek 2.py
@@ -0,0 +1,19 @@
+def print_this(str):
+    print str
+
+
+def jumlah(a, b):
+    return a + b
+
+
+# Buatlah fungsi pengurangan, perkalian, pembagian
+
+
+name = "Joko Widodo"
+
+
+print_this("My name is " + name)
+
+print jumlah(0, 3)
+
+# Panggil dan tampilkan hasil dari fungsi pengurangan, perkalian, pembagian
